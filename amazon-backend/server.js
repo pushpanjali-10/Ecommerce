4b5cb988fe22cf96487bf66fda4f04bb4ef737cb
Hari Narayan <harinarayan947255@gmail.com>
2023-08-17 13:58:36 +0530

    first

--- amazon-backend/server.js
+++ amazon-backend/server.js
@@ -4,13 +4,21 @@ import cors from 'cors'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
 import dotenv from 'dotenv'
+import { Router } from 'express'
 import orderRouter from './routers/orderRouter.js'
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
 
 
 dotenv.config();
-
+express.Router();
+// Router.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 const app = express()
-const port = process.env.PORT || 5000;
+const port = 5000;
 const connection_url = process.env.MONGO_URL;
 
 mongoose.connect(connection_url,{
@@ -22,9 +30,31 @@ mongoose.connect(connection_url,{
 
 
 app.use(express.json());
-app.use(cors());
+// var cors = require('cors')
+
+// app.use(cors()) // Use this after the variable declaration
+
+// app.use(cors());
+const corsOrigin ={
+    origin:'http://localhost:3000', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 
 
+// app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       target: 'http://localhost:3000',
+//       changeOrigin: true,
+//       pathRewrite: {
+//         '^/api': '', // Remove '/api' from the request path
+//       },
+//     })
+//   );
+
+  
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
